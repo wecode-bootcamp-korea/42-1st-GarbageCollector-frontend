3f62901b169f888475e1411876b60a7abe34f9ec
이해인 <ihaein@ihaein-ui-MacBookPro.local>
2023-02-07 14:57:39 +0900

    ADD:Product구현

--- src/pages/ProductList/Product/Product.js
+++ src/pages/ProductList/Product/Product.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Product.scss';
 
-const Product = () => {
-  return <div />;
+const Product = ({ props }) => {
+  const [productChange, setProductChange] = useState(false);
+  const { productPic, useProductPic, productName, price, productAlt } = props;
+
+  return (
+    <ul>
+      <li Link to="#">
+        <img
+          onMouseOver={() => setProductChange(true)}
+          onMouseOut={() => setProductChange(false)}
+          className="itemPicture"
+          src={productChange ? { useProductPic } : { productPic }}
+          alt={productAlt}
+        />
+        <span className="productName">{productName}</span>
+        <span className="productPrice">{price}</span>
+      </li>
+    </ul>
+  );
 };
 
 export default Product;
--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Product from './Product/Product';
 import './ProductList.scss';
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/heinMock.json')
+      .then(response => response.json())
+      .then(data => setProductList(data));
+  }, []);
+
+  return (
+    <div>
+      {productList.map((product, id) => (
+        <Product key={id} props={product} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
