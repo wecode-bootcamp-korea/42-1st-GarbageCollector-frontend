d754f0e33c32414549278948c981a4597faf4b45
gmgmgun <gmgmgun@gmail.com>
2023-02-13 16:23:40 +0900

    Add: Carousel 컴포넌트 추가

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,52 +1,116 @@
 import React, { useEffect, useState } from 'react';
-import Slide from './Slide/Slide';
 import './Carousel.scss';
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [slideList, setSlideList] = useState([]);
-
-  const onClickPrevious = () => {
-    setCurrentIndex(
-      currentIndex === 0 ? slideList.length - 1 : currentIndex - 1
-    );
-  };
-
-  const onClickNext = () => {
-    setCurrentIndex(
-      currentIndex === slideList.length - 1 ? 0 : currentIndex + 1
-    );
-  };
+  const [action, setAction] = useState(true);
+  const [isTransition, setIsTransition] = useState(false);
 
   useEffect(() => {
-    fetch('/data/carousel.json')
-      .then(result => result.json())
-      .then(data => setSlideList(data));
+    setSlideList(IMAGE_LIST);
+    setSlideList(prev => [
+      IMAGE_LIST[IMAGE_LIST.length - 1],
+      ...prev,
+      IMAGE_LIST[0],
+    ]);
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => setCurrentIndex(currentIndex + 1), 4000);
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsTransition(false);
+    }, 1000);
+
+    const startIndex = 0;
+    currentIndex === startIndex &&
+      setTimeout(() => {
+        setAction(0);
+        setCurrentIndex(3);
+        setTimeout(() => {
+          setAction(1);
+        }, 20);
+      }, 1000);
+    currentIndex === slideList.length - 1 &&
+      setTimeout(() => {
+        setAction(0);
+        setCurrentIndex(1);
+        setTimeout(() => {
+          setAction(1);
+        }, 20);
+      }, 1000);
+    return () => {};
+  }, [currentIndex, slideList.length]);
+
+  const clickHandler = ({ target }) => {
+    setCurrentIndex(Number(target.innerText));
+  };
+
+  const onClickButton = ({ target }) => {
+    if (isTransition) return;
+    setAction(1);
+    target.className === 'previous'
+      ? setCurrentIndex(curIndex => {
+          setIsTransition(true);
+          return curIndex - 1;
+        })
+      : setCurrentIndex(curIndex => {
+          setIsTransition(true);
+          return curIndex + 1;
+        });
+  };
 
   return (
     <div className="carousel">
       <ul
         className="slide-list"
         style={{
-          transform: `translateX(${
-            (-100 / slideList.length) * (0.5 + currentIndex)
-          }%)`,
+          width: `${100 * slideList.length - 1}vw`,
+          transform: `translateX(${-100 * currentIndex}vw)`,
+          transition: `all ${action}s`,
         }}
       >
-        {slideList.map(slide => (
-          <li key={slide.id}>
-            <Slide slide={slide} />
-          </li>
+        {slideList.map(({ src }, idx) => (
+          <img key={idx} alt="Slide" src={src} />
         ))}
       </ul>
-      <button onClick={onClickPrevious}>
-        <img alt="<" src="images/main_arr_prev.png" />
+      <button className="previous-button" onClick={onClickButton}>
+        <img className="previous" alt="<" src="images/main_arr_prev.png" />
       </button>
-      <button onClick={onClickNext}>
-        <img alt=">" src="images/main_arr_next.png" />
+      <button className="next-button" onClick={onClickButton}>
+        <img className="next" alt=">" src="images/main_arr_next.png" />
+      </button>
+      <button className="index-button" onClick={clickHandler}>
+        {Array(IMAGE_LIST.length)
+          .fill()
+          .map((_, idx) => (
+            <span
+              key={idx}
+              className={currentIndex === idx + 1 ? 'active' : ''}
+            >
+              {idx + 1}
+            </span>
+          ))}
       </button>
     </div>
   );
 };
 export default Carousel;
+
+const IMAGE_LIST = [
+  {
+    src: 'https://cdn-mart.baemin.com/inventory-unit/c219f0b9-0674-453a-a8a2-7a89ffbd3bf0.png',
+  },
+  {
+    src: 'https://cdn-mart.baemin.com/inventory-unit/24fee219-24f3-479d-8918-95536b469cfb.jpg',
+  },
+  {
+    src: 'https://cdn-mart.baemin.com/inventory-unit/ceb02ee2-d489-4101-8dd7-0b44f7145196.png',
+  },
+];
