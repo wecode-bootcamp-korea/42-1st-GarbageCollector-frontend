7ee75b71cfe6b0a66b82cfdb12095b4f5f96905f
이해인 <ihaein@ihaein-ui-MacBookPro.local>
2023-02-15 00:36:54 +0900

    MODIFY : 리뷰반영

--- src/pages/ProductDetail/ProductOrderModal.js
+++ src/pages/ProductDetail/ProductOrderModal.js
@@ -1,35 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const ProductOrderModal = () => {
-  const [productQuantity, setProductQuantity] = useState(1);
-
-  const handleMinus = () => {
-    setProductQuantity(productQuantity - 1);
-  };
-  const handlePlus = () => {
-    setProductQuantity(productQuantity + 1);
-  };
-
-  return (
-    <div className="buy-list-box">
-      <h4>상품이름</h4>
-      <div className="buy-options">
-        <div className="buy-quantity">
-          <button onClick={handleMinus} className="btn-minus">
-            -
-          </button>
-          <input type="number" />
-          <button onClick={handlePlus} className="btn-plus">
-            +
-          </button>
-        </div>
-        <p>
-          <span>가격</span>
-        </p>
-      </div>
-      <button className="btn-delete" />
-    </div>
-  );
+  return <div />;
 };
 
 export default ProductOrderModal;
